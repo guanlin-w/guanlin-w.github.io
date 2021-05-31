7d8d19e133f80be801d78bff03a4d73f8b03aaac
guanlin-w <guanlinwee01@gmail.com>
2021-05-31 15:18:24 -0500

    need to fix drawer content when scaled down

---
+++ js/drawer.js
@@ -0,0 +1,74 @@
+
+/* script required for the animated dropdropdown */
+/* some relevant css styling is in the css file */
+
+/* drawer-container is always present and is the activator for the drawer */
+/* drawer-content is the content of the drawer that is expanded or collapsed */
+
+
+
+/* the click action that triggers when drawer-container is clicked */
+function clicked()
+{
+    var drawerButtonId = this.id;
+    var drawer = getCorrespondingDrawer(document.getElementsByClassName("drawer-content"),drawerButtonId);
+    if (drawer.classList.toggle('expanded'))
+    {
+        drawer.style.marginTop = '0px';
+    }
+    if (drawer.classList.toggle('collapsed')){
+        drawer.style.marginTop = '-'+drawer.offsetHeight +'px';
+    }
+}
+
+
+/* helper function that matches the drawer-container and drawer-content */
+function getCorrespondingDrawer (list, id){
+
+    for (var i = 0; i < list.length; i++){
+        if(list[i].id == id){
+            return list[i];
+        }
+    }
+    return null;
+}
+
+
+
+
+
+/* sets up the event listeners */
+document.addEventListener("DOMContentLoaded",function(e) {
+
+     var drawerButtons = document.getElementsByClassName("drawer-container");
+     var drawerList = document.getElementsByClassName("drawer-content");
+
+    setTimeout(function(){;},10);
+    for (var i = 0; i < drawerButtons.length; i++){
+
+        var button = drawerButtons.item(i);
+
+        var drawer = getCorrespondingDrawer(drawerList, button.id);
+        if (drawer != null) {
+            
+            button.addEventListener("click",clicked);
+            
+            /*TODO: can append the other styles to this */
+            console.log(drawer.offsetHeight);
+        }
+    }
+
+    /* need small delay */
+    /* if this is done in the loop above without the delay, some instances
+        of the page will have a negative offset of 74 instead of its actual
+        height*/
+    setTimeout(function(){var drawerList = document.getElementsByClassName("drawer-content");
+        for(var i = 0; i < drawerList.length;i++){
+            drawerList[i].style.marginTop = '-'+drawerList[i].offsetHeight+'px'; 
+        }}, 10);
+
+
+});
+
+
+
